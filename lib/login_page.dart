e4394edc83a37e289f95291b9ce9aff0119b617d
Gabriel <gabriel.avlis.123@gmail.com>
2023-05-31 17:55:12 -0300

    31/05

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -17,110 +17,129 @@ class _Login_pageState extends State<Login_page> {
   final emailed = GlobalKey<FormFieldState>();
 
   Widget _body() {
-    return Column(
-      children: [
-        SingleChildScrollView(
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 200,
-                    height: 200,
-                    child: Image.asset('assets/images/login3.png'),
-                  ),
-                  Container(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    key: emailed,
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Coloque o email';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    // ignore: prefer_const_constructors
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: const OutlineInputBorder(),
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          SingleChildScrollView(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: Image.asset('assets/images/icons.png'),
                     ),
-                  ),
-                  // ignore: prefer_const_constructors
-                  SizedBox(height: 10),
-                  TextFormField(
-                    onChanged: (text) {
-                      password = text;
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Coloque senha';
-                      }
-                      return null;
-                    },
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      // ignore: prefer_const_constructors
-                      border: OutlineInputBorder(),
+                    Container(height: 10),
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          children: [
+                            SingleChildScrollView(
+                              child: TextFormField(
+                                key: emailed,
+                                onChanged: (text) {
+                                  email = text;
+                                },
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Coloque o email';
+                                  }
+                                  return null;
+                                },
+                                keyboardType: TextInputType.emailAddress,
+                                // ignore: prefer_const_constructors
+                                decoration: InputDecoration(
+                                  labelText: 'Email',
+                                  border: const OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                            // ignore: prefer_const_constructors
+                            SizedBox(height: 10),
+                            TextFormField(
+                              onChanged: (text) {
+                                password = text;
+                              },
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Coloque senha';
+                                }
+                                return null;
+                              },
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                                // ignore: prefer_const_constructors
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            ElevatedButton(
+                              onPressed: () {
+                                _formKey.currentState!.validate();
+                                if (email == 'gabriel@gmail.com' &&
+                                    password == '123') {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const HomePage(),
+                                    ),
+                                  );
+                                } else {
+                                  // ignore: avoid_print
+                                  print('Login invalido');
+                                }
+                              },
+                              child: const SizedBox(
+                                width: double.infinity,
+                                child: Text(
+                                  'Entrar',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                emailed.currentState?.validate();
+                              },
+                              child: const Text('Esqueceu sua senha ?'),
+                            )
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      _formKey.currentState!.validate();
-                      if (email == 'gabriel@gmail.com' && password == '123') {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomePage()));
-                      } else {
-                        // ignore: avoid_print
-                        print('Login invalido');
-                      }
-                    },
-                    child: const Text('Entrar'),
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        emailed.currentState?.validate();
-                      },
-                      child: const Text('Esqueceu sua senha ?')),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Scaffold(
-        body: Stack(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height,
-              child: Image.asset(
-                'assets/images/backgroud.png',
-                fit: BoxFit.fill,
-              ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/brown.jpg',
+              fit: BoxFit.cover,
             ),
-            Container(color: Colors.black.withOpacity(0.3)),
-            _body(),
-          ],
-        ),
+          ),
+          //Container(color: Colors.black.withOpacity(0.3)),
+          _body(),
+        ],
       ),
     );
   }
